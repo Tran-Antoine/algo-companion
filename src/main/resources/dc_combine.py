0dca922f8c1c36815715eb990402331d6bf2584c
Antoine Tran <antoine2001.tran@gmail.com>
2022-07-16 23:27:36 +0200

    Complete assembly procedure of base case, division and combining

---
+++ src/main/resources/dc_combine.py
@@ -0,0 +1,8 @@
+def combine(n, *args):
+    # create shortcut variables to make user code shorter
+    if n > 0: arg0 = args[0]
+    if n > 1: arg1 = args[1]
+    if n > 2: arg2 = args[2]
+    if n > 3: arg3 = args[3]
+    if n > 4: arg4 = args[4]
+    if n > 5: arg5 = args[5]
--- src/test/scala/ch/epfl/alcmp/scripting/DCAssemblerTest.scala
+++ src/test/scala/ch/epfl/alcmp/scripting/DCAssemblerTest.scala
@@ -1,13 +1,34 @@
 package ch.epfl.alcmp.scripting
 
+import ch.epfl.alcmp.scripting.DCAssembler.{assemble, getClass}
 import org.scalatest.flatspec.AnyFlatSpec
-import org.scalatest._
-import matchers._
+import org.scalatest.*
+import matchers.*
+
+import java.io.{FileInputStream, FileOutputStream, FileWriter, InputStream}
+import scala.io.Source
 
 class DCAssemblerTest extends AnyFlatSpec with should.Matchers {
 
   "Reading content" should "return null string iff path is invalid" in {
-    DCAssembler.readTemplate("foo.py") should be (null)
+    DCAssembler.readContent("foo.py") should be (null)
     DCAssembler.readTemplate should not be (null)
   }
+
+  "Adding line to function" should "indent correctly" in {
+    val insertion = DCAssembler.insertCode("def print2(n):", "print(n)")
+    val expected  =  read("indent_example.txt")
+
+    insertion should be (expected)
+  }
+
+  "Assembly of python script" should "be the correct script" in {
+
+    val assembly = DCAssembler.assembleUserFunctions("return n == 1", "return arg[0:n//2], arg[n//2:n]", "return arg0 + arg1")
+    val expected = read("assembly_example.txt")
+
+    assembly should be (expected)
+  }
+
+  private def read(name: String): String = Source.fromInputStream(new FileInputStream("src/test/resources/" + name)).mkString
 }
