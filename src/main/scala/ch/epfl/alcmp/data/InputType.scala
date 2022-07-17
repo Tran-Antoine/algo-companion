b36182368d81dcac72fedeeed547dc74ef37eb09
Antoine Tran <antoine2001.tran@gmail.com>
2022-07-17 22:34:56 +0200

    Move type aliases to the companion object

--- src/main/scala/ch/epfl/alcmp/data/InputType.scala
+++ src/main/scala/ch/epfl/alcmp/data/InputType.scala
@@ -2,11 +2,14 @@ package ch.epfl.alcmp.data
 
 sealed trait InputType
 
-trait BinaryTree[T]
-case class Leaf[T]() extends BinaryTree[T]
-case class Node[T](value: T, left: BinaryTree[T], right: BinaryTree[T]) extends BinaryTree[T]
+object InputType {
+  trait BinaryTree[T]
+  case class Leaf[T]() extends BinaryTree[T]
+  case class Node[T](value: T, left: BinaryTree[T], right: BinaryTree[T]) extends BinaryTree[T]
 
-type IList = List[Int] with InputType
-type IMatrix = List[List[Int]] with InputType
-type IHeap = List[Int] with InputType
-type IBinaryTree = BinaryTree[Int] with InputType
+  type IList = List[Int] with InputType
+  type IMatrix = List[List[Int]] with InputType
+  type IHeap = List[Int] with InputType
+  type IBinaryTree = BinaryTree[Int] with InputType
+}
+
