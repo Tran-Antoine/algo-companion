38ba9ab18d6f4dcd026995c79675438510c4c472
Antoine Tran <antoine2001.tran@gmail.com>
2022-07-17 22:22:21 +0200

    Set up network messages and data structures
    Add simple Serdes for primitive types with their test
    
    Add case classes for messaging
    
    Add type aliases for input types

---
+++ src/main/scala/ch/epfl/alcmp/net/Serde.scala
@@ -0,0 +1,23 @@
+package ch.epfl.alcmp.net
+
+import java.nio.charset.StandardCharsets
+import java.util.Base64
+
+trait Serde[T] {
+
+  def serialize(obj: T): String
+  def deserialize(data: String): T
+}
+
+object Serde {
+
+  val NUMBER: Serde[Int] = new Serde[Int] {
+    override def serialize(obj: Int): String = String.valueOf(obj)
+    override def deserialize(data: String): Int = Integer.parseInt(data)
+  }
+
+  val TEXT: Serde[String] = new Serde[String] {
+    override def serialize(obj: String): String = Base64.getEncoder.encodeToString(obj.getBytes(StandardCharsets.UTF_8))
+    override def deserialize(data: String): String = new String(Base64.getDecoder.decode(data), StandardCharsets.UTF_8)
+  }
+}
