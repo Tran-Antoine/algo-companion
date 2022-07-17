cb0551b1f03c123b788fcaacc7c31b69bf748d0e
Antoine Tran <antoine2001.tran@gmail.com>
2022-07-17 22:34:06 +0200

    Add template for division messages

--- src/main/scala/ch/epfl/alcmp/net/Serde.scala
+++ src/main/scala/ch/epfl/alcmp/net/Serde.scala
@@ -1,5 +1,8 @@
 package ch.epfl.alcmp.net
 
+import ch.epfl.alcmp.data.InputType
+import ch.epfl.alcmp.data.InputType.{IBinaryTree, IHeap, IList, IMatrix}
+
 import java.nio.charset.StandardCharsets
 import java.util.Base64
 
@@ -20,4 +23,11 @@ object Serde {
     override def serialize(obj: String): String = Base64.getEncoder.encodeToString(obj.getBytes(StandardCharsets.UTF_8))
     override def deserialize(data: String): String = new String(Base64.getDecoder.decode(data), StandardCharsets.UTF_8)
   }
+
+  val REGISTER_MESSAGE: Serde[RegisterMessage] = ???
+
+  val DIVIDE_LIST_MESSAGE: Serde[DivideMessage[IList]] = ???
+  val DIVIDE_MATRIX_MESSAGE: Serde[DivideMessage[IMatrix]] = ???
+  val DIVIDE_HEAP_MESSAGE: Serde[DivideMessage[IHeap]] = ???
+  val DIVIDE_BINARY_TREE_MESSAGE: Serde[DivideMessage[IBinaryTree]] = ???
 }
