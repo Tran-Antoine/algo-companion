9a724b0835efdc6b870feeabe5ba1e10f4245c45
Antoine Tran <antoine2001.tran@gmail.com>
2022-07-19 14:19:33 +0200

    Set up architecture for sockets
    Add TypeId enum for deserialization purposes
    
    Make Serde objects implicit

--- src/main/scala/ch/epfl/alcmp/data/InputType.scala
+++ src/main/scala/ch/epfl/alcmp/data/InputType.scala
@@ -1,15 +1,31 @@
 package ch.epfl.alcmp.data
 
-sealed trait InputType
+sealed trait InputType {
+  def getType: TypeId
+}
 
-object InputType {
-  trait BinaryTree[T]
-  case class Leaf[T]() extends BinaryTree[T]
-  case class Node[T](value: T, left: BinaryTree[T], right: BinaryTree[T]) extends BinaryTree[T]
+trait BinaryTree[T]
+case class Leaf[T]() extends BinaryTree[T]
+case class Node[T](value: T, left: BinaryTree[T], right: BinaryTree[T]) extends BinaryTree[T]
 
-  type IList = List[Int] with InputType
-  type IMatrix = List[List[Int]] with InputType
-  type IHeap = List[Int] with InputType
-  type IBinaryTree = BinaryTree[Int] with InputType
+
+case class IList(list: List[Int]) extends InputType {
+  override def getType: TypeId = TypeId.ListType
+}
+
+case class IMatrix(rows: List[List[Int]]) extends InputType {
+  override def getType: TypeId = TypeId.MatrixType
+}
+
+case class IHeap(list: List[Int]) extends InputType {
+  override def getType: TypeId = TypeId.HeapType
+}
+
+case class IBinaryTree(root: BinaryTree[Int]) extends InputType {
+  override def getType: TypeId = TypeId.BinaryTreeType
+}
+
+enum TypeId {
+  case ListType, MatrixType, HeapType, BinaryTreeType
 }
 
