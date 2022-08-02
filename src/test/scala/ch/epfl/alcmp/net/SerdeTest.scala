2398511013f53562f22df20beb014eec8576e370
AzTekG <88374025+AzTekG@users.noreply.github.com>
2022-08-02 17:49:31 +0200

    Add tests for COMBINE message

--- src/test/scala/ch/epfl/alcmp/net/SerdeTest.scala
+++ src/test/scala/ch/epfl/alcmp/net/SerdeTest.scala
@@ -1,5 +1,7 @@
 package ch.epfl.alcmp.net
 
+import ch.epfl.alcmp.data.IList
+import ch.epfl.alcmp.data.TypeId
 import ch.epfl.alcmp.net.SimulationMessage.{CombineMessage, DivideMessage, DoneMessage, RegisterMessage}
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should
@@ -25,14 +27,24 @@ class SerdeTest extends AnyFlatSpec with should.Matchers {
   }
 
   "Serializing and Deserializing REGISTER messages" should "work" in {
-    val reg1 = new RegisterMessage(57)
+    val reg1 = RegisterMessage(57)
     Serdes.serialize[RegisterMessage](reg1) should be ("57")
     Serdes.deserialize[RegisterMessage]("68") should be (RegisterMessage(68))
   }
 
   "Serializing and Deserializing DONE messages" should "work" in {
-    val reg1 = new DoneMessage(57456)
+    val reg1 = DoneMessage(57456)
     Serdes.serialize[DoneMessage](reg1) should be ("57456")
     Serdes.deserialize[DoneMessage]("68") should be (DoneMessage(68))
+  }
+
+  "Serializing and Deserializing COMBINE messages" should "work" in {
+    val reg1 = CombineMessage(101, 2, 1, IList(List(5, 6, 7)), List(0, 1, 2))
+    val reg2 = CombineMessage(102, 3, 2, IList(List(6, 7, 8)), List(1, 2, 3))
+    Serdes.serialize[CombineMessage](TypeId.ListType, reg1) should be ("101/2/1/5,6,7/0,1,2")
+    Serdes.serialize[CombineMessage](TypeId.ListType, reg2) should be ("102/3/2/6,7,8/1,2,3")
+    Serdes.deserialize[CombineMessage](TypeId.ListType, "1/2/3/0,9,10/0,1,2") should be (CombineMessage(1, 2, 3, IList(List(0, 9, 10)), List(0, 1, 2)))
+    Serdes.deserialize[CombineMessage](TypeId.ListType, "0/0/0/0,0,0/0,0,0") should be (CombineMessage(0, 0, 0, IList(List(0, 0, 0)), List(0, 0, 0)))
+
   }
 }
