b21cd262fd0e83a7af24c9f59c9809406edd7ac0
Antoine Tran <antoine2001.tran@gmail.com>
2022-08-02 21:33:08 +0800

    Make request handling loop until a done message is received and the map is empty

--- src/main/scala/ch/epfl/alcmp/net/SimulationListener.scala
+++ src/main/scala/ch/epfl/alcmp/net/SimulationListener.scala
@@ -1,33 +1,48 @@
 package ch.epfl.alcmp.net
 
 
-import ch.epfl.alcmp.data.{InputType, TypeId}
+import ch.epfl.alcmp.data.{InputType, SimulationResult, TypeId}
 import ch.epfl.alcmp.net.SimulationMessage.{CombineMessage, DivideMessage, DoneMessage, RegisterMessage}
 
 import java.io.{BufferedReader, InputStreamReader}
 import java.net.Socket
 import java.nio.charset.StandardCharsets
 import java.util.regex.Pattern
+import scala.collection.mutable
 
 class SimulationListener(private val socket: Socket, private val typeId: TypeId) {
 
-  def run(): Unit =
+  type SimulationId = Int
 
+  var ongoingSimulations: Map[SimulationId, SimulationResult] = Map.empty
+
+  given BufferedReader =
     val inputStream = socket.getInputStream
-    val reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.US_ASCII))
+    new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.US_ASCII))
 
-    def handleOne(): Unit =
-      val args = reader.readLine().split(Pattern.quote(" "), -1)
-      val content = args(1)
-      MessageId.valueOf(args(0)) match {
-        case MessageId.REGISTER => handleRegister(Serdes.deserialize[RegisterMessage](content))
-        case MessageId.DONE     => handleDone(Serdes.deserialize[DoneMessage](content))
-        case MessageId.COMBINE  => handleCombine(Serdes.deserialize[CombineMessage](typeId, content))
-        case MessageId.DIVIDE   => handleDivide(Serdes.deserialize[DivideMessage](typeId, content))
-      }
+  def handleOne(using reader: BufferedReader): Unit =
+    val args = reader.readLine().split(Pattern.quote(" "), -1)
+    val content = args(1)
+    MessageId.valueOf(args(0)) match {
+      case MessageId.REGISTER => handleRegister(Serdes.deserialize[RegisterMessage](content))
+      case MessageId.DONE     => handleDone(Serdes.deserialize[DoneMessage](content))
+      case MessageId.COMBINE  => handleCombine(Serdes.deserialize[CombineMessage](typeId, content))
+      case MessageId.DIVIDE   => handleDivide(Serdes.deserialize[DivideMessage](typeId, content))
+    }
 
-  def handleRegister(message: RegisterMessage): Unit = ???
-  def handleDone(message: DoneMessage): Unit = ???
+  def run(): Unit =
+    handleOne
+
+  def handleRegister(message: RegisterMessage): Unit =
+    ongoingSimulations += (message.id, new SimulationResult)
+    handleOne
+
+  def handleDone(message: DoneMessage): Unit =
+    ongoingSimulations -= message.id
+    if ongoingSimulations.isEmpty then finish else handleOne
+
   def handleDivide(message: DivideMessage): Unit = ???
   def handleCombine(message: CombineMessage): Unit = ???
+
+  def finish: Unit = ???
 }
