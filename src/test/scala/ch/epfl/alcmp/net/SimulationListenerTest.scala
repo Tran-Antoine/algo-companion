54b6a17017025c60e7d6840e16a3c982718a23a2
Antoine Tran <antoine2001.tran@gmail.com>
2022-08-06 17:26:56 +0800

    Add complex test case with 2 Out-Of-Order simulations
    Fix sockets not being closed after use

--- src/test/scala/ch/epfl/alcmp/net/SimulationListenerTest.scala
+++ src/test/scala/ch/epfl/alcmp/net/SimulationListenerTest.scala
@@ -29,6 +29,9 @@ class SimulationListenerTest extends AsyncFlatSpec with should.Matchers {
     writer.flush()
     writer.close()
 
+    receiver.close()
+    server.close()
+
     task map {
       result =>
         result.size should be (1)
@@ -81,6 +84,9 @@ class SimulationListenerTest extends AsyncFlatSpec with should.Matchers {
     writer.flush()
     writer.close()
 
+    receiver.close()
+    server.close()
+
     task map {
       result =>
         result.size should be (1)
@@ -91,6 +97,113 @@ class SimulationListenerTest extends AsyncFlatSpec with should.Matchers {
         head.divisionRowAt(0) should be (List(input))
         head.divisionValueAt(1, 0) should be (left)
         head.divisionValueAt(1, 1) should be (right)
+    }
+  }
+
+  "Multiple simulation in random order" should "produce multiple correct SimulationData objects" in {
+    val server = ServerSocket(5108)
+
+    val receiver = Socket("localhost", 5108)
+    val sender = server.accept()
+
+    val task: Future[List[SimulationData]] = new SimulationListener(receiver, TypeId.ListType).run()
+
+    val writer = new BufferedWriter(new OutputStreamWriter(sender.getOutputStream))
+
+
+    val input1 =  IList(List(1,2,3,4))
+    val (left1, right1) = (IList(List(1,2)), IList(List(3,4)))
+    val (oLeft1, oRight1) = (IList(List(2)), IList(List(4)))
+    val output1 = IList(List(4))
+
+    val input2 =  IList(List(5,6,7,8))
+    val (left2, right2) = (IList(List(5,6)), IList(List(7,8)))
+    val (oLeft2, oRight2) = (IList(List(6)), IList(List(8)))
+    val output2 = IList(List(8))
+
+    writer.write(s"REGISTER ${Serdes.serialize[RegisterMessage](RegisterMessage(30))}\n")
+
+
+    writer.write(s"DIVIDE ${Serdes.serialize[DivideMessage](
+      TypeId.ListType,
+      DivideMessage(30, 0, 0, List(input1), Nil))}\n")
+
+    writer.write(s"DIVIDE ${Serdes.serialize[DivideMessage](
+      TypeId.ListType,
+      DivideMessage(30, 1, 0, List(left1), Nil))}\n")
+
+
+    writer.write(s"REGISTER ${Serdes.serialize[RegisterMessage](RegisterMessage(31))}\n")
+
+
+    writer.write(s"DIVIDE ${Serdes.serialize[DivideMessage](
+      TypeId.ListType,
+      DivideMessage(31, 1, 0, List(left2), Nil))}\n")
+
+
+    writer.write(s"COMBINE ${Serdes.serialize[CombineMessage](
+      TypeId.ListType,
+      CombineMessage(30, 1, 0, oLeft1, Nil))}\n")
+
+    writer.write(s"DIVIDE ${Serdes.serialize[DivideMessage](
+      TypeId.ListType,
+      DivideMessage(31, 0, 0, List(input2), Nil))}\n")
+
+    writer.write(s"DIVIDE ${Serdes.serialize[DivideMessage](
+      TypeId.ListType,
+      DivideMessage(31, 1, 1, List(right2), Nil))}\n")
+
+    writer.write(s"DIVIDE ${Serdes.serialize[DivideMessage](
+      TypeId.ListType,
+      DivideMessage(30, 1, 1, List(right1), Nil))}\n")
+
+    writer.write(s"COMBINE ${Serdes.serialize[CombineMessage](
+      TypeId.ListType,
+      CombineMessage(31, 1, 0, oLeft2, Nil))}\n")
+
+    writer.write(s"COMBINE ${Serdes.serialize[CombineMessage](
+      TypeId.ListType,
+      CombineMessage(31, 1, 1, oRight2, Nil))}\n")
+
+    writer.write(s"COMBINE ${Serdes.serialize[CombineMessage](
+      TypeId.ListType,
+      CombineMessage(31, 0, 0, output2, Nil))}\n")
+
+    writer.write(s"DONE ${Serdes.serialize[DoneMessage](DoneMessage(31))}\n")
+
+    writer.write(s"COMBINE ${Serdes.serialize[CombineMessage](
+      TypeId.ListType,
+      CombineMessage(30, 1, 1, oRight1, Nil))}\n")
+
+    writer.write(s"COMBINE ${Serdes.serialize[CombineMessage](
+      TypeId.ListType,
+      CombineMessage(30, 0, 0, output1, Nil))}\n")
+
+    writer.write(s"DONE ${Serdes.serialize[DoneMessage](DoneMessage(30))}\n")
+
+
+    writer.flush()
+    writer.close()
+
+    receiver.close()
+    server.close()
+
+    task map {
+      result =>
+        result.size should be (2)
+
+        val r1 = result.head
+        val r2 = result.tail.head
+
+        r1.combineValueAt(0, 0) should be (output1)
+        r1.divisionRowAt(0) should be (List(input1))
+        r1.divisionValueAt(1, 0) should be (left1)
+        r1.divisionValueAt(1, 1) should be (right1)
+
+        r2.combineValueAt(0, 0) should be (output2)
+        r2.divisionRowAt(0) should be (List(input2))
+        r2.divisionValueAt(1, 0) should be (left2)
+        r2.divisionValueAt(1, 1) should be (right2)
     }
   }
 }
