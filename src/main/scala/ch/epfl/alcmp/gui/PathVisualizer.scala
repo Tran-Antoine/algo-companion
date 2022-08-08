dcb7a9d70642c9de38a63e3fe2a82ca1b42c158a
Antoine Tran <antoine2001.tran@gmail.com>
2022-08-08 11:26:04 +0800

    Fix CompositeVisualizer by delaying animation play call

--- src/main/scala/ch/epfl/alcmp/gui/PathVisualizer.scala
+++ src/main/scala/ch/epfl/alcmp/gui/PathVisualizer.scala
@@ -10,6 +10,7 @@ object PathVisualizer extends Visualizer[VisualizableLine] {
   override def visualize(parent: Pane, line: VisualizableLine, pos: Position): Animation =
     val lineObj = Line(line.start.x, line.start.y, line.start.x, line.start.y)
     lineObj.setStrokeWidth(2.5)
+    lineObj.setOpacity(0)
 
     parent.getChildren.add(lineObj)
 
@@ -18,13 +19,12 @@ object PathVisualizer extends Visualizer[VisualizableLine] {
         setCycleDuration(Duration.millis(1000))
       }
       override def interpolate(frac: Double): Unit =
+        lineObj.setOpacity(1)
         val vector = (line.end.x - line.start.x, line.end.y - line.start.y)
         val forward = (frac * vector._1, frac * vector._2)
 
         lineObj.setEndX(line.start.x + forward._1)
         lineObj.setEndY(line.start.y + forward._2)
     }
-
-    animation.play()
     animation
 }
