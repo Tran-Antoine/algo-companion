1c1f37ec153a4b6c28941e82dbde2bc33bb4ded3
Antoine Tran <antoine2001.tran@gmail.com>
2022-08-08 11:15:26 +0800

    Use Sequential and Parallel transitions for composite animation

--- src/main/scala/ch/epfl/alcmp/gui/CompositeVisualizer.scala
+++ src/main/scala/ch/epfl/alcmp/gui/CompositeVisualizer.scala
@@ -1,6 +1,6 @@
 package ch.epfl.alcmp.gui
 
-import javafx.animation.{Animation, Transition}
+import javafx.animation.{Animation, ParallelTransition, SequentialTransition, Transition}
 import javafx.scene.layout.Pane
 import javafx.scene.Node
 import javafx.util.Duration
@@ -10,32 +10,20 @@ object CompositeVisualizer {
   def drawWithPath[T <: Visualizable](using v1: Visualizer[T])
                                      (pane: Pane, top: T, links: List[T], topPos: Position, linksPos: List[Position]): Animation =
 
-    // TODO: use CompositeTransition + ParallelTransition instead
-    val animation: Transition = new Transition() {
-      var started = false
-      var reachedThird = false
-      var reachedSecondThird = false
-      val shiftedTopPosition = Position(topPos.x, topPos.y + 60)
+    val anim1 = v1.visualize(pane, top, topPos)
 
-      {
-        setCycleDuration(Duration.millis(3000))
-      }
-      override def interpolate(v: Double): Unit =
-        if v >= 0 && !started then
-          started = true
-          v1.visualize(pane, top, topPos)
-        else if v >= 0.33 && !reachedThird then
-          reachedThird = true
-          for (pos <- linksPos) {
-            PathVisualizer.visualize(pane, (shiftedTopPosition, pos), topPos)
-          }
-        else if v >= 0.66 && !reachedSecondThird then
-          reachedSecondThird = true
-          for ((link, linkPos) <- links.zip(linksPos)) {
-            v1.visualize(pane, link, linkPos)
-          }
+    val shiftedTopPosition = Position(topPos.x, topPos.y + 60)
+    val anim2 = ParallelTransition()
+    for (pos <- linksPos) {
+      anim2.getChildren.add(PathVisualizer.visualize(pane, (shiftedTopPosition, pos), topPos))
     }
 
-    animation.play()
-    animation
+    val anim3 = SequentialTransition()
+    for ((link, linkPos) <- links.zip(linksPos)) {
+      anim3.getChildren.add(v1.visualize(pane, link, linkPos))
+    }
+
+    val fullAnimation = SequentialTransition(anim1, anim2, anim3)
+    fullAnimation.play()
+    fullAnimation
 }
