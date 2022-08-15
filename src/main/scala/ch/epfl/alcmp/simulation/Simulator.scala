8c6c8f1917832dcc2af99bca03799449a77cc6b6
AzTekG <88374025+AzTekG@users.noreply.github.com>
2022-08-15 08:30:04 +0200

    Add method to copy files

--- src/main/scala/ch/epfl/alcmp/simulation/Simulator.scala
+++ src/main/scala/ch/epfl/alcmp/simulation/Simulator.scala
@@ -2,11 +2,13 @@ package ch.epfl.alcmp.simulation
 
 import ch.epfl.alcmp.scripting.DCAssembler
 
-import java.io.{File, FileWriter}
+import java.io.{File, FileInputStream, FileOutputStream, FileReader, FileWriter, IOException}
+import scala.io.Source
 
 class Simulator {
 
   private val BIN_FOLDER = "src/main/bin/"
+  private val PYTHON_FOLDER = "src/main/python/"
 
   def makeDCScript(base: String, div: String, comb: String): Unit =
     val script = DCAssembler.assemble(base, div, comb)
@@ -14,6 +16,14 @@ class Simulator {
     writer.write(script)
     writer.close()
 
+  def copyToBin(fileName: String): Unit =
+    val src = PYTHON_FOLDER + fileName
+    val dest = BIN_FOLDER + fileName
+    val inputChannel = new FileInputStream(src).getChannel
+    val outputChannel = new FileOutputStream(dest).getChannel
+    outputChannel.transferFrom(inputChannel, 0, inputChannel.size())
+    inputChannel.close()
+    outputChannel.close()
 
 
 }
