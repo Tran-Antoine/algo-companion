67eed700ce680c3ccbdee4657dbd98e6e176e9d1
AzTekG <88374025+AzTekG@users.noreply.github.com>
2022-08-15 08:30:33 +0200

    Add test for file copying method

--- src/test/scala/ch/epfl/alcmp/simulation/SimulatorTest.scala
+++ src/test/scala/ch/epfl/alcmp/simulation/SimulatorTest.scala
@@ -2,16 +2,26 @@ package ch.epfl.alcmp.simulation
 
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should
+import java.io.File
 
 class SimulatorTest extends AnyFlatSpec with should.Matchers {
 
-    "Creating dc script" should "work perfectly fine" in {
+    val simulator = new Simulator()
+
+    "Creating dc script" should "work" in {
 
       val divide = "return arg[:n//2], arg[n//2:]"
       val combine = "return [arg0[0]] if arg0[0] > arg1[0] else [arg1[0]]"
       val base = "if n == 1: return arg"
 
-      val simulator = new Simulator()
       simulator.makeDCScript(base, divide, combine)
+
+      assert(File("src/main/bin/DC_script.py").exists())
+    }
+
+    "Copying matrix file to bin folder" should "work" in {
+      simulator.copyToBin("/matrix.py")
+
+      assert(File("src/main/bin/matrix.py").exists())
     }
 }
