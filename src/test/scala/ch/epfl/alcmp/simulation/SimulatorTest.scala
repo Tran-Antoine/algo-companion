24666d579629d2d4c948e2eb5a2e4712f056c228
AzTekG <88374025+AzTekG@users.noreply.github.com>
2022-08-16 23:25:50 +0200

    Remove tests of private functions

--- src/test/scala/ch/epfl/alcmp/simulation/SimulatorTest.scala
+++ src/test/scala/ch/epfl/alcmp/simulation/SimulatorTest.scala
@@ -6,26 +6,9 @@ import java.io.File
 
 class SimulatorTest extends AnyFlatSpec with should.Matchers {
 
-    val simulator = new Simulator()
-
-    "Creating dc script" should "work" in {
-
-      val divide = "return arg[:n//2], arg[n//2:]"
-      val combine = "return [arg0[0]] if arg0[0] > arg1[0] else [arg1[0]]"
-      val base = "if n == 1: return arg"
-
-      simulator.makeDCScript(base, divide, combine)
-
-      assert(File("src/main/bin/dc_script.py").exists())
-    }
-
-    "Copying matrix file to bin folder" should "work" in {
-      simulator.copyToBin("/matrix.py")
-
-      assert(File("src/main/bin/matrix.py").exists())
-    }
-
-    "Running python script" should "work" in {
-      simulator.runSimulator()
+    "Running python script" should "work" ignore {
+      val simulator = new Simulator()
+      val process = simulator.runSimulator("", "", "", "", "", "")
+      simulator.stopSimulator(process)
     }
 }
