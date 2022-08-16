2a79c47fec9c2235ed88b3bda9a153e3c04409f2
AzTekG <88374025+AzTekG@users.noreply.github.com>
2022-08-16 23:25:27 +0200

    Add helper auxiliary methods

--- src/main/scala/ch/epfl/alcmp/simulation/Simulator.scala
+++ src/main/scala/ch/epfl/alcmp/simulation/Simulator.scala
@@ -1,8 +1,12 @@
 package ch.epfl.alcmp.simulation
 
+import ch.epfl.alcmp.data.TypeId
+import ch.epfl.alcmp.data.TypeId.BinaryTreeType
+import ch.epfl.alcmp.net.SimulationListener
 import ch.epfl.alcmp.scripting.DCAssembler
 
 import java.io.{BufferedReader, File, FileInputStream, FileOutputStream, FileReader, FileWriter, IOException, InputStreamReader}
+import java.net.Socket
 import scala.io.Source
 
 class Simulator {
@@ -10,26 +14,42 @@ class Simulator {
   private val BIN_FOLDER = "src/main/bin/"
   private val PYTHON_FOLDER = "src/main/python/"
 
-  def makeDCScript(base: String, div: String, comb: String): Unit =
+  private def makeDCScript(base: String, div: String, comb: String): Unit =
     val script = DCAssembler.assemble(base, div, comb)
     val writer: FileWriter = new FileWriter(new File(BIN_FOLDER + "dc_script.py"))
     writer.write(script)
     writer.close()
 
-  def copyToBin(fileName: String): Unit =
-    val src = PYTHON_FOLDER + fileName
-    val dest = BIN_FOLDER + fileName
+  private def copyToBin(file: String): Unit =
+    val src = PYTHON_FOLDER+file
+    val dest = BIN_FOLDER+file
     val inputChannel = new FileInputStream(src).getChannel
     val outputChannel = new FileOutputStream(dest).getChannel
     outputChannel.transferFrom(inputChannel, 0, inputChannel.size())
     inputChannel.close()
     outputChannel.close()
 
-  def runSimulator(): Unit =
+  private def getType(type_id: String): TypeId =
+    if type_id == "ListType"       then return TypeId.ListType
+    if type_id == "MatrixType"     then return TypeId.MatrixType
+    if type_id == "BinaryTreeType" then return TypeId.BinaryTreeType
+    if type_id == "HeapType"       then return TypeId.HeapType
+    else throw new IllegalArgumentException
+
+  def runSimulator(base: String, div: String, comb: String,
+                   typeId: String, port: String, dc_arg: String): Process =
+    makeDCScript(base, div, comb)
     copyToBin("simulator.py")
-    val process = ProcessBuilder("python", PYTHON_FOLDER + "simulator.py").start()
-    val reader = new BufferedReader(new InputStreamReader(process.getInputStream))
-    print(reader.readLine())
+    copyToBin("serde.py")
+    copyToBin("matrix.py") //TODO how to know when to copy this
+
+    val listener = new SimulationListener(new Socket("localhost", port.toInt), getType(typeId))
+
+    val process = ProcessBuilder("python", PYTHON_FOLDER+"simulator.py", typeId, port, dc_arg).start()
+    process
+    //TODO this solution seems a bit meh
+
+  def stopSimulator(process: Process): Unit =
     process.destroy()
 
 }
