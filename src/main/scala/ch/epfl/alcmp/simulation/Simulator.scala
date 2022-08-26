1233b5649a2c910c9233f099f54ab3a73422b2c2
AzTekG <88374025+AzTekG@users.noreply.github.com>
2022-08-26 11:33:38 +0200

    Remove stopSimulator method

--- src/main/scala/ch/epfl/alcmp/simulation/Simulator.scala
+++ src/main/scala/ch/epfl/alcmp/simulation/Simulator.scala
@@ -1,12 +1,13 @@
 package ch.epfl.alcmp.simulation
 
-import ch.epfl.alcmp.data.TypeId
+import ch.epfl.alcmp.data.{SimulationData, TypeId}
 import ch.epfl.alcmp.data.TypeId.BinaryTreeType
 import ch.epfl.alcmp.net.SimulationListener
 import ch.epfl.alcmp.scripting.DCAssembler
 
 import java.io.{BufferedReader, File, FileInputStream, FileOutputStream, FileReader, FileWriter, IOException, InputStreamReader}
-import java.net.Socket
+import java.net.{ServerSocket, Socket}
+import scala.concurrent.Future
 import scala.io.Source
 
 class Simulator {
@@ -36,6 +37,7 @@ class Simulator {
     if type_id == "HeapType"       then return TypeId.HeapType
     else throw new IllegalArgumentException
 
+  
   def runSimulator(base: String, div: String, comb: String,
                    typeId: String, port: String, dc_arg: String): Process =
     makeDCScript(base, div, comb)
@@ -43,13 +45,13 @@ class Simulator {
     copyToBin("serde.py")
     copyToBin("matrix.py") //TODO how to know when to copy this
 
+    val server = ServerSocket(port.toInt)
     val listener = new SimulationListener(new Socket("localhost", port.toInt), getType(typeId))
+    val sender = server.accept()
+    val task: Future[List[SimulationData]] = listener.run()
+    print(task)
 
     val process = ProcessBuilder("python", PYTHON_FOLDER+"simulator.py", typeId, port, dc_arg).start()
     process
-    //TODO this solution seems a bit meh
-
-  def stopSimulator(process: Process): Unit =
-    process.destroy()
 
 }
