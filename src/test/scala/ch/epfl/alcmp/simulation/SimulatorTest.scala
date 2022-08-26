ca8c51ec7f4696c92c4860b54af7fc94410f6bdf
AzTekG <88374025+AzTekG@users.noreply.github.com>
2022-08-26 11:34:08 +0200

    Add test arguments

--- src/test/scala/ch/epfl/alcmp/simulation/SimulatorTest.scala
+++ src/test/scala/ch/epfl/alcmp/simulation/SimulatorTest.scala
@@ -6,9 +6,13 @@ import java.io.File
 
 class SimulatorTest extends AnyFlatSpec with should.Matchers {
 
-  "Running python script" should "work" ignore {
+  "Running python script" should "work" in {
+    val input = "5,8,3,7,10,23,16,2"
+    val divide = "return arg[:n//2], arg[n//2:]"
+    val combine = "return [arg0[0]] if arg0[0] > arg1[0] else [arg1[0]]"
+    val base = "if n == 1: return arg"
     val simulator = new Simulator()
-    val process = simulator.runSimulator("", "", "", "", "", "")
-    simulator.stopSimulator(process)
+    val process = simulator.runSimulator(base, divide, combine, "ListType", "3999", input)
+    process.destroy()
   }
 }
